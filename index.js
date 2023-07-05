735f17cd3b720739fb570b78c813bc9145f2ad67
Miguel-PC1317 <85039271+MiguelDomingues-Dev@users.noreply.github.com>
2023-07-05 16:55:48 -0300

    atualizando dados

--- index.js
+++ index.js
@@ -8,25 +8,32 @@ function enter() {
     var def = 500
     var sh = 250
     var ph = 5000
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
     if (atk > def && sh == 0) {
         var dano = atk - def
         var pt = ph - dano
-        res.innerHTML = `Venon | Defesa 0 | Escudo: 0 | ${pt}Ph`
+        img.setAttribute('src', 'venon.jpg')
+        res.innerHTML = `Venon | Defesa ${dano} | Escudo: 0 | ${pt}Ph`
+        res2.innerText = `${char} teve um ataque de ${atk}ATK e não conseguiu derrotar o BOSS`
     } else if (atk > def && sh == 250) {
-        var dano = atk - sh / 2 - def
-        var pt = ph - dano 
+        var dano = atk - sh 
+        var dan = dano - def / 2
+        var pt = ph - dan
+        img.setAttribute('src', 'burns.jpg')
         if (pt != 0) {
-            res.innerHTML = `Venon | Defesa 0 | Escudo: 0 | ${pt}Ph`
+            res.innerHTML = `Venon | Defxesa 0 | Escudo: 0 | ${pt}Ph`
             res2.innerText = `${char} teve um ataque de ${atk}ATK e não conseguiu derrotar o BOSS`
         } else if (pt == 0) {
             res2.innerText = `${char} teve um ataque de ${atk}ATK e conseguiu derrotar o BOSS`
             res.innerHTML = `Venon | Defesa 0 | Escudo: 0 | 0Ph`
+            img.setAttribute('src', 'homer.jpg')
         }
     } else {
         atk > def || atk == def 
-        var dano = 0
+        img.setAttribute('src', 'venon.jpg')
         res.innerHTML = `Venon | Defesa: 500 | Escudo: 250 | 5000Ph`
     }
     res.style.textAlign = 'center'    
     res2.style.textAlign = 'center'
-}
+    res.appendChild(img)}
