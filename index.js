6477eb8b5e997ed160d085777f555f0c5f1654aa
MiguelDomingues-Dev <85039271+MiguelDomingues-Dev@users.noreply.github.com>
2023-07-16 19:06:21 -0300

    atualizando as funções

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-function enter() {
-    var ataque = document.getElementById('atk')
-    var character = document.getElementById('name-character1')
-    var res = document.getElementById('res')
-    var res2 = document.getElementById('resposta')
-    var char = (character.value)
-    var atk = Number(ataque.value)
-    var def = 500
-    var sh = 250
-    var ph = 5000
-    var img = document.createElement('img')
-    img.setAttribute('id', 'foto')
-    if (atk > def && sh == 0 && res.style.display == 'none' && res2.style.display == 'none') {
-            res.style.display = 'block'
-            res2.style.display = 'block'
-            img.setAttribute('src', 'venon.jpg')
-            var dano = atk - def
-            var pt = ph - dano
-            res.innerHTML = `Venon | Defesa ${dano} | Escudo: 0 | ${pt}Ph`
-            res2.innerText = `${char} teve um ataque de ${atk}ATK e não conseguiu derrotar o BOSS`
-    } else if (atk > def && sh == 250) {
-            res.style.display = 'block'
-            res2.style.display = 'block'
-            var dano = atk - sh 
-            var dan = dano - def / 2
-            var pt = ph - dan
-        img.setAttribute('src', 'burns.jpg')
-        if (pt != 0) {
-            res.style.display = 'block'
-            res2.style.display = 'block'
-            res.innerHTML = `Venon | Defxesa 0 | Escudo: 0 | ${pt}Ph`
-            res2.innerText = `${char} teve um ataque de ${atk}ATK e não conseguiu derrotar o BOSS`
-        } else if (pt == 0) {
-            res.style.display = 'block'
-            res2.style.display = 'block'
-            res2.innerText = `${char} teve um ataque de ${atk}ATK e conseguiu derrotar o BOSS`
-            res.innerHTML = `Venon | Defesa 0 | Escudo: 0 | 0Ph`
-            img.setAttribute('src', 'homer.jpg')
+const vilao = {
+        vida: 5000,
+        defesa: 500,
+        shield: 250
+}
+
+const char = document.getElementById('name-character1')
+const res = document.getElementById('res')
+const res2 = document.getElementById('resposta')
+const vel = document.getElementById('vilao')
+
+function lutem(powerAtaqueUSer) {
+        if (vilao.shield >= powerAtaqueUSer) {
+                vilao.shield -= powerAtaqueUSer
+                console.log(`${powerAtaqueUSer} ${vilao.shield}`)
+        } else if (vilao.shield > 0) {
+                const danoAposEscudo = vilao.shield - vilao.shield
+                vilao.shield = 0
+                vilao.defesa -= danoAposEscudo
+                console.log(`Escudo zerado ${vilao.shield}, Falta passar pela a ${vilao.defesa} defesa. Vida restante ${vilao.vida}`)
+        } else if (vilao.defesa > 0) {
+                const danoAposDefesa = vilao.defesa - powerAtaqueUSer
+                vilao.defesa = 0
+                vilao.vida -= danoAposDefesa
+                console.log(`Escudo zerado, defesa zerado ${vilao.defesa}! Vida restante ${vilao.vida}`)
+        } else {
+                vilao.vida - powerAtaqueUSer
+                console.log(`Vida restante ${vilao.vida}`)
+        } 
+        if (vilao.vida <= 0) {
+                console.log('vilao Derrotado!')
+                res.innerText = `O heroi ${char} com um poder de ${powerAtaqueUSer} Conseguiu derrotar o Boss`
         }
-    } else {
-            res.style.display = 'block'
-            res2.style.display = 'block'
-            atk > def || atk == def 
-            img.setAttribute('src', 'venon.jpg')
-            res.innerHTML = `Venon | Defesa: 500 | Escudo: 250 | 5000Ph`
-    }
-    res.style.textAlign = 'center'    
-    res2.style.textAlign = 'center'
-    res.appendChild(img)}
+
+}
+
+function enter() {
+        const powerAtaqueUSer = parseInt(document.getElementById('atk').value)
+        if (res.style.display == 'none' && res2.style.display == 'none' && vel.style.display == 'block') {
+                res.style.display = 'block'
+                res2.style.display = 'block'
+                res.style.display = 'none'
+        }
+        lutem(powerAtaqueUSer)
+        res.style.textAlign = 'center'    
+        res2.style.textAlign = 'center'
+} 
+
+
