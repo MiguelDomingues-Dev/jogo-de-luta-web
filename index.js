ef9a365629d27d6dd62a979df7b5c13230725e3b
MiguelDomingues-Dev <85039271+MiguelDomingues-Dev@users.noreply.github.com>
2023-07-23 17:33:24 -0300

    commit add funções

--- index.js
+++ index.js
@@ -16,70 +16,57 @@ function calcularDano(powerAtaqueUSer) {
 //criação das variaveis das resposta do programa
 //Função para processeguir o procedimento de ataque e subtrair uma por uma em cada variavel e com base na sobra ele desconta na proxima variavel
 function lutem(powerAtaqueUSer) {
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        function imgResposta(img) {
-                const resImg = document.getElementById('res-img')
-                if (vilao.shield == 0) {
-                        img.setAttribute('src', 'burns.jpg')
-                } else if (vilao.defesa == 0) {
-                        img.setAttribute('src', 'homer.jpg')
-                }
-                resImg.appendChild(img)
-                resImg.style.alignItems = 'center'
-        }
-        const res = document.getElementById('res')
-        const res2 = document.getElementById('resposta')
-        const vel = document.getElementById('vilao')
-        const char = document.getElementById('name-character1').value
-        if (vilao.shield >= powerAtaqueUSer && res.style.display == 'none' && res2.style.display == 'none' && vel.style.display == 'block') {
+        const vidaVilao = document.getElementById('barra-vida2')
+        const vidaVilaoEscudo = document.getElementById('barra-vida-3')
+        const vidaVilaoDefesa = document.getElementById('barra-vida-4')
+        const vidaVilaoVida = document.getElementById('barra-vida-5')
+        const ryuLutador = document.getElementById('lutador')
+        const socoUser = document.getElementById('ryu-user-soco')
+
+        if (vilao.shield >= powerAtaqueUSer && vidaVilao.style.display === 'block' && vidaVilaoEscudo.style.display === 'none' && vidaVilaoDefesa.style.display === 'none' && vidaVilaoVida.style.display === 'none' && ryuLutador.style.display === 'block' && socoUser.style.display === 'none') {
                 vilao.shield -= powerAtaqueUSer
-                res.style.display = 'block'
-                res2.style.display = 'block'
-                vel.style.display = 'none'
+                vidaVilao.style.display = 'none'
+                vidaVilaoEscudo.style.display = 'block'
+                vidaVilaoDefesa.style.display = 'none'
+                vidaVilaoVida.style.display = 'none'
+                ryuLutador.style.display = 'none'
+                socoUser.style.display = 'block'
                 console.log(`${powerAtaqueUSer} ${vilao.shield}`)
-                res.innerHTML = `O heroi ${char} com um poder de ${powerAtaqueUSer} ainda não conseguiu derrotar o Boss`
-                res2.innerText = `${powerAtaqueUSer} ${vilao.shield}`
         } else if (vilao.shield > 0) {
                 const danoAposEscudo = vilao.shield - vilao.shield
                 vilao.shield = 0
                 vilao.defesa -= danoAposEscudo
-                res.style.display = 'block'
-                res2.style.display = 'block'
-                vel.style.display = 'none'
+                vidaVilao.style.display = 'none'
+                vidaVilaoEscudo.style.display = 'block'
+                vidaVilaoDefesa.style.display = 'none'
+                vidaVilaoVida.style.display = 'none'
+                ryuLutador.style.display = 'none'
+                socoUser.style.display = 'block'
                 console.log(`Escudo zerado ${vilao.shield}, Falta passar pela a ${vilao.defesa} defesa. Vida restante ${vilao.vida}`)
-                res.innerHTML = `O heroi ${char} com um poder de ${powerAtaqueUSer} ainda não conseguiu derrotar o Boss`
-                res2.innerText = `Escudo zerado ${vilao.shield}, Falta passar pela a ${vilao.defesa} defesa. Vida restante ${vilao.vida}`
         } else if (vilao.defesa > 0) {
                 const danoAposDefesa = vilao.defesa - powerAtaqueUSer
+                vidaVilao.style.display = 'none'
+                vidaVilaoEscudo.style.display = 'none'
+                vidaVilaoDefesa.style.display = 'block'
+                vidaVilaoVida.style.display = 'none'
+                ryuLutador.style.display = 'none'
+                socoUser.style.display = 'block'
                 vilao.defesa = 0
                 vilao.vida -= danoAposDefesa
-                res.style.display = 'block'
-                res2.style.display = 'block'
-                vel.style.display = 'none'
                 console.log(`Escudo zerado, defesa zerado ${vilao.defesa}! Vida restante ${vilao.vida}`)
-                res.innerHTML = `O heroi ${char} com um poder de ${powerAtaqueUSer} ainda não conseguiu derrotar o Boss`
-                res2.innerText = `Escudo zerado, defesa zerado ${vilao.defesa}! Vida restante ${vilao.vida}`
         } else {
                 vilao.vida -= powerAtaqueUSer    
-                res.style.display = 'block'
-                res2.style.display = 'block'
-                vel.style.display = 'none'
                 console.log(`Ataque de ${powerAtaqueUSer}! Vida restante: ${vilao.vida}`)
-                res.innerHTML = `O heroi ${char} com um poder de ${powerAtaqueUSer} ainda não conseguiu derrotar o Boss`
-                res2.innerText = `Ataque de ${powerAtaqueUSer}! Vida restante: ${vilao.vida}`
         } 
         if (vilao.vida <= 0) {
-                res.style.display = 'block'
-                res2.style.display = 'block'
-                vel.style.display = 'none'
+                vidaVilao.style.display = 'none'
+                vidaVilaoEscudo.style.display = 'none'
+                vidaVilaoDefesa.style.display = 'none'
+                vidaVilaoVida.style.display = 'block'
+                ryuLutador.style.display = 'none'
+                socoUser.style.display = 'block'
                 console.log('vilao Derrotado!')
-                res.innerHTML = `O heroi ${char} com um poder de ${powerAtaqueUSer} conseguiu derrotar o Boss`
-                res2.innerText = `Ataque de ${powerAtaqueUSer}! Vida restante: ${vilao.vida}`
         }
-        res.style.textAlign = 'center'    
-        res2.style.textAlign = 'center'
-        imgResposta(img)
 }
 
 //Função criada para puxar as outras funções e criar a ação de click no botom do doc HTML e executar o código.
